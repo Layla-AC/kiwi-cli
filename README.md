b833b37c518d2ff0ac2ab02b65ecb42792db3329
Trent Charles <trentacharles@gmail.com>
2018-02-26 21:49:37 -0500

    1.0.0

--- README.md
+++ README.md
@@ -4,4 +4,64 @@ Node.js CLI that allows for the developer to include any arbitrary list of comma
 
 This Repository is not particularly designed to be usable at the moment - it may or may not receive active development later.
 
-Work in Progress
+### Setup
+
+To use Kiwi-CLI, you need to `require` the module in your code, and then call the exported function.
+
+The startup function requires 3 parameters:
+* (String) Application Name
+* (Object) Commands
+* (Function) Error Handler
+
+Application name is simply the name the prompter will display when waiting for user input.
+
+The Commands object is meant to be an object of functions that are run when various commands are used on the CLI. Each function in the object
+is it's own command and will be usable as such. Arguments are passed to commands by taking the input string and splitting it along every space
+between characters. It is recommended to use the spread operator if a command allows for arguments, and then iterating through the array to
+parse them. Command function return values should be falsy in the case of no errors occuring- truthy values will be treated as errors and
+passed to your error handler function.
+
+The Error Handler function is a function that will be called whenever a command function returns a truthy value. The Error Handler will be called
+with the truthy value being passed as a parameter to it.
+
+### Example Usage
+
+```
+	// Include the 2 main exports
+	const kiwi = require("kiwi-cli");
+	
+	// Get our commands object
+	// This is best handled by storing them in another file
+	// However, for this example I'll define them here
+	// Commands are expected to return false for successful operations
+	// or an error if something goes wrong
+	const commands = {
+		foo: function(...args) {
+			if (args) {
+				console.log("bar");
+				return false;
+			} else {
+				return ("Error");
+			}
+		},
+		
+		help: function() {
+			console.log("Hello World");
+			return false;
+		}
+	};
+	
+	// Define our error handler
+	// This is called whenever one of our commands returns a truthy value
+	function catchErr(err, fatal) {
+		console.log(err);
+		if (fatal) process.exit();
+	}
+	
+	// We're ready to start handling input from the user
+	// The initialization function will return a reference
+	// To the prompt function - which usually won't be needed.
+	const prompt = kiwi("My Application", commands, catchErr);
+```
+
+If you have any questions or concerns, you can reach me at trentacharles@gmail.com
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 const chalk = require("chalk");
+process.stdin.setEncoding("utf8");
 
 class Operation {
 	constructor(id, method) {
@@ -19,42 +20,105 @@ class CLI_MANAGER {
 	constructor(appname, commands) {
 		this.appname = appname;
 		this.commands = commands;
+		this.cli = require("readline").createInterface({
+			input: process.stdin,
+			output: process.stdout,
+			completer: this.completer,
+			terminal: true,
+			historySize: 100,
+			prompt: `[${this.appname}]$`,
+			removeHistoryDuplicates: true
+		});
+		this.cli.on("SIGINT",() => {process.exit();});
+		this.cli.on("SIGCONT",() => {this.prompt()});
 	}
+	
+	prompt() { this.cli.prompt(); }
 	
 	fetch(id) {
 		id = id.trim();
 		if (this.commands[id]) return new Operation(id, this.commands[id]);
 		return null;
 	},
+	
+	matcher(str,set) {
+		if (set.indexOf(str) > -1) return null;
+		function toId(str) { return String(str).toLowerCase().replace(/[^a-z0-9]+/g, ""); }
+		for (let i = 0; i < set.length; i++) {
+			if (set[i].startsWith(toId(str))) return set[i];
+		}
+		let d = 0;
+		const R = [2,4,6,8];
+		for (d; d < R.length; d++) {
+			if (str.length <= R[d]) break;
+		}
+		function calc(s,t,l) {
+			let d = [];
+			let n = s.length;
+			let m = t.length;
+			if (n === 0) return m;
+			if (m === 0) return n;
+			if (l && Math.abs(m - n) > l) return Math.abs(m - n);
+			for (let i = n; i >= 0; i--) d[i] = [];
+			for (let i = n; i >= 0; i--) d[i][0] = i;
+			for (let j = m; j >= 0; j--) d[0][j] = j;
+			for (let i = 1; i <= n; i++) {
+				let s_i = s.charAt(i - 1);
+				for (let j = 1; j <= m; j++) {
+					if (i === j && d[i][j] > 4) return n;
+					let t_j = t.charAt(j - 1);
+					let cost = (s_i === t_j) ? 0 : 1;
+					let mi = d[i - 1][j] + 1;
+					let b = d[i][j - 1] + 1;
+					let c = d[i - 1][j - 1] + cost;
+					if (b < mi) mi = b;
+					if (c < mi) mi = c;
+					d[i][j] = mi;
+				}
+			}
+			return d[n][m];
+		}
+		for (const i of set) {
+			if (calc(toId(str),i,d) <= d) return [i];
+		}
+		return null;
+	}
+	
+	completer(input) {
+		const match = this.matcher(input,Object.getOwnPropertyNames(this.commands));
+		if (!match) {
+			return [[],input];
+		} else if (typeof match === "string") {
+			return [[match],input];
+		} else if (typeof match === "object") {
+			this.cli.write(null,{ctrl:true,name:'u'});
+			this.cli.write(match[0]);
+			return [];
+		}
+	}
 
 	noOp(cmd) {
 		console.log(chalk.red.bold("--Invalid Command: " + cmd + " - type `help` for a list of commands."));
 		this.prompt();
 	}
-	
-	prompt() {
-		process.stdout.write("[" + this.appname + "]$ ");
-		process.stdin.resume();
-	}
 };
 
-function acceptInput(appname, commands, errFunction) {
+function init(appname, commands, e) {
 	const Manager = new CLI_MANAGER(appname, commands);
 	Manager.prompt();
-	process.stdin.setEncoding('utf8');
-	process.stdin.on("data", async function(input) {
+	Manager.cli.on("line", async function(input) {
 		const args = input.split(",");
 		const cmd = args.shift().trim();
 		const operation = Manager.fetch(cmd);
 		if (!operation) return Manager.noOp(cmd);
 		const err = await operation.run(this, args);
 		if (err) {
-			errFunction(err, Manager.prompt);
+			e(err);
 		} else {
 			Manager.prompt();
 		}
 	}.bind(this));
+	return Manager.prompt;
 }
 
-exports.prompt = prompt;
-exports.acceptInput = acceptInput;
+module.exports = init;
